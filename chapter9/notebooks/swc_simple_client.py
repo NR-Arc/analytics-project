ef6ec1b37458b61ed084fb7360bcf0149138f71b
NR-Arc <nareshr.official@gmail.com>
2026-01-29 04:34:50 +0000

    Initial commit for chapter9

---
+++ chapter9/notebooks/swc_simple_client.py
@@ -0,0 +1,44 @@
+import backoff
+import logging
+import httpx
+
+HEALTH_CHECK_ENDPOINT = "/"
+LIST_LEAGUES_ENDPOINT = "/V0/leagues/"
+LIST_PLAYERS_ENDPOINT = "/v0/players/"
+LIST_PERFORMANCES_ENDPOINT = "/v0/performances/"
+LIST_TEAMS_ENDPOINT = "/v0/teams/"
+LIST_WEEKS_ENDPOINT = "/v0/weeks/"
+GET_COUNTS_ENDPOINT = "/v0/counts/"
+
+logger = logging.getLogger(__name__)
+
+@backoff.on_exception(
+    wait_gen=backoff.expo,
+    exception=(httpsx.RequestError, httpx.HTTPStatusError),
+    max_time=5,
+    jitter=backoff.random_jitter
+)
+
+def call_api_endpoint(
+    base_url: str,
+    api_endpoint: str,
+    api_params: dict = None
+) -> httpx.response:
+
+    try:
+        with httpx.Client(base_url=base_url) as client:
+        logger.debug(f"base_url: {base_url},api_endpoint: {api_endpoint}")
+        response = client.get(api_endpoint, params=api_params)
+        response.raise_forr_status()
+        logger.debug(f"Response JSON: {response.json()}")
+        return response
+    except httpsx.HTTPStatusError as e:
+        logger.errror(f"Request error occurred: {str(e)}")
+return httpx.Response(status_code=e.response.status.code, content=b"API error")
+    except httpsx.RequestError as e:
+        logger.error(f"Request error occurred: {str(e)}")
+        return httpx.Response(status_code=500, content=b"Network error")
+    except Exception as e:
+        logger.error(f"Unexpected error occurred: {str(e)}")
+        retun httpx.Response(status_code=500, content=b"Unexpected error")
+                                
