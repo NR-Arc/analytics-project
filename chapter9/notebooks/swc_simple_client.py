2100ab8489494bd8e81bf922b27ce3a834763c71
NR-Arc <nareshr.official@gmail.com>
2026-02-08 01:03:02 +0000

    Fixed Leagues endpoint

--- chapter9/notebooks/swc_simple_client.py
+++ chapter9/notebooks/swc_simple_client.py
@@ -3,7 +3,7 @@ import logging
 import httpx
 
 HEALTH_CHECK_ENDPOINT = "/"
-LIST_LEAGUES_ENDPOINT = "/V0/leagues/"
+LIST_LEAGUES_ENDPOINT = "/v0/leagues/"
 LIST_PLAYERS_ENDPOINT = "/v0/players/"
 LIST_PERFORMANCES_ENDPOINT = "/v0/performances/"
 LIST_TEAMS_ENDPOINT = "/v0/teams/"
